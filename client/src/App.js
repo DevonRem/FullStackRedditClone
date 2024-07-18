e0ece111e51ea8880d4a4c8a4b65a546da4104ab
DevonRem <DevonReminick@gmail.com>
2024-07-18 14:18:28 -0400

    header/search bar frontend done

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import Logo from './Dlogo.png';
+import {SearchIcon}from '@heroicons/react/solid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="flex w-full bg-redditDark p-2">
+        <div className='mx-4'>
+          <img src={Logo} alt="" className='w-8 h-8 '/>
+        </div>
+        <form action='' className='bg-redditDark-brighter p-1 px-3 flex rounded-md border border-gray-700'>
+          <SearchIcon className="text-gray-300 h-6 w-6"/>
+          <input type='text' className='bg-redditDark-brighter text-sm p-1 pl-2 pr-0 block focus: outline-none text-white' placeholder='Search'></input>
+        </form>
       </header>
     </div>
   );
