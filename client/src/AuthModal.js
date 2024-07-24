607d43e522cc46c05791f3d4c05225846f203053
DevonRem <DevonReminick@gmail.com>
2024-07-24 14:27:59 -0400

    Login/register modal + api imports

--- client/src/AuthModal.js
+++ client/src/AuthModal.js
@@ -4,7 +4,13 @@ import {useState } from 'react';
 
 function AuthModal() {
     const [modalType, setModalType] = useState('login');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
+    function register(e) {
+
+    }
 
     return(
         <div className="w-screen h-screen fixed top-0 left-0 z-20 flex" style={{backgroundColor:`rgba(0,0,0,.8)`}}>
@@ -16,18 +22,31 @@ function AuthModal() {
                     <h1 className="text-2xl mb-3">Register</h1>
                 )}
 
+                {modalType === 'register' && (
+                    <label>
+                        <span className="">Email:</span>
+                        <Input type="email" className="mb-2 w-full" value={email} onChange={e => setEmail(e.target.value)}></Input>
+                    </label>
+                )}
+
                 <label>
                     <span className="">Username:</span>
-                    <Input type="text" className="mb-2 w-full"></Input>
+                    <Input type="text" className="mb-2 w-full" value={username} onChange={e => setUsername(e.target.value)}></Input>
                 </label>
 
                 <label>
                     <span className="">Password: </span>
-                    <Input type="password" className="mb-2 w-full"></Input>
+                    <Input type="password" className="mb-2 w-full" value={password} onChange={e => setPassword(e.target.value)}></Input>
                 </label>
 
-                <button className="border border-gray-300 bg-gray-300 mt-2 mb-1 rounded-full px-3 py-1 font-bold w-full text-black">Login</button>
-                
+                {modalType === 'login' && (
+                    <button className="border border-gray-300 bg-gray-300 mt-2 mb-1 rounded-full px-3 py-1 font-bold w-full text-black">Login</button>
+                )}
+                {modalType === 'register' && (
+                    <button className="border border-gray-300 bg-gray-300 mt-2 mb-1 rounded-full px-3 py-1 font-bold w-full text-black">Sign up</button>
+                )}
+        
+
                 {modalType === 'login' && (
                     <div>
                     New user? <button className='text-blue-600 mt-1' onClick={()=> setModalType('register')}>Sign Up!</button>
